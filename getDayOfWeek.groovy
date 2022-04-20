39de2492b52c3a86eab08f0ab301ecd7f7d866ad
jazzGitHb <104098268+jazzGitHb@users.noreply.github.com>
2022-04-20 15:36:06 -0400

    Delete getDayOfWeek.groovy

--- getDayOfWeek.groovy
+++
@@ -1,7 +0,0 @@
-def call(){
-  def now = new Date()
-  echo now.format("yyMMdd.HHmm", TimeZone.getTimeZone('UTC'))
-  Calender cal = Calender.getInstance();
-  day = now[cal.DAY_OF_WEEK]
-  return day.toString()
-}
