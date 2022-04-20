dd1b6003ed5474eaf7715f8b16d2457386d8b80d
jazzGitHb <104098268+jazzGitHb@users.noreply.github.com>
2022-04-20 15:35:48 -0400

    Update getDayOfWeek.groovy

--- vars/getDayOfWeek.groovy
+++ vars/getDayOfWeek.groovy
@@ -1,1 +1,7 @@
-a
+def call(){
+  def now = new Date()
+  echo now.format("yyMMdd.HHmm", TimeZone.getTimeZone('UTC'))
+  Calender cal = Calender.getInstance();
+  day = now[cal.DAY_OF_WEEK]
+  return day.toString()
+}
